dea9130d829eaa2795318c5734c0fe2c6a6aed19
Clifford William <ccliffordwilliam@gmail.com>
2024-02-01 15:35:49 +0800

    feat(deploy): fe deploy url

--- client/src/c.ts
+++ client/src/c.ts
@@ -1,2 +1,2 @@
 export const BASE_BE_URL = "https://william-love-hotel.as.r.appspot.com";
-export const BASE_FE_URL = "http://localhost:3000";
+export const BASE_FE_URL = "https://python-library-fabula-client.vercel.app";
